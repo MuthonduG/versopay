9986fc0171a19e042de96e23420389088ec1a744
wametee <josephwamiti8711@gmail.com>
2026-02-17 17:57:44 +0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 
 import './index.css'
+import FeaturesPage from './pages/home/features/features'
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Welcome to Versopay
-      </h1>
+     <FeaturesPage />
 
     </>
   )
