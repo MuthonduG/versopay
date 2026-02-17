9624511a10b994aee154389cdac4f61eeff999de
MuthonduG <muthondugithinji@gmail.com>
2026-02-17 21:42:35 +0300

    updates: removed unused imports for navbar

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactElement } from 'react'
+// import React, { type ReactElement } from 'react'
 import NavbarComponent from '../../components/layout/navigation/navbar/NavbarComponent'
 
 
